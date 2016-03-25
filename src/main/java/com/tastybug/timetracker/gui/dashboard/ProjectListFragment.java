dfb80fc3322395c3cdc936a3b087f9bcc784d576
Philipp Bartsch <tastybug@tastybug.com>
2016-03-26 00:08:16 +0100

    progress #34: project view has a working tracking button

--- src/main/java/com/tastybug/timetracker/gui/dashboard/ProjectListFragment.java
+++ src/main/java/com/tastybug/timetracker/gui/dashboard/ProjectListFragment.java
@@ -8,9 +8,15 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 
+import com.squareup.otto.Subscribe;
 import com.tastybug.timetracker.R;
 import com.tastybug.timetracker.gui.dialog.ProjectCreationDialog;
 import com.tastybug.timetracker.model.Project;
+import com.tastybug.timetracker.task.OttoProvider;
+import com.tastybug.timetracker.task.tracking.CreatedTrackingRecordEvent;
+import com.tastybug.timetracker.task.tracking.KickStartedTrackingRecordEvent;
+import com.tastybug.timetracker.task.tracking.KickStoppedTrackingRecordEvent;
+import com.tastybug.timetracker.task.tracking.ModifiedTrackingRecordEvent;
 
 public class ProjectListFragment extends ListFragment {
 
@@ -24,6 +30,13 @@ public class ProjectListFragment extends ListFragment {
     public void onResume() {
         super.onResume();
         setListAdapter(new ProjectListAdapter(getActivity()));
+        new OttoProvider().getSharedBus().register(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        new OttoProvider().getSharedBus().unregister(this);
     }
 
     @Override
@@ -55,5 +68,22 @@ public class ProjectListFragment extends ListFragment {
     public interface ProjectListSelectionListener {
 
         void onProjectSelected(Project project);
+    }
+
+    @Subscribe
+    public void handleTrackingCreated(CreatedTrackingRecordEvent event) {
+        ((ProjectListAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+    @Subscribe public void handleTrackingKickStarted(KickStartedTrackingRecordEvent event) {
+        ((ProjectListAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+    @Subscribe public void handleTrackingModified(ModifiedTrackingRecordEvent event) {
+        ((ProjectListAdapter)getListAdapter()).notifyDataSetChanged();
+    }
+
+    @Subscribe public void handleTrackingKickStopped(KickStoppedTrackingRecordEvent event) {
+        ((ProjectListAdapter)getListAdapter()).notifyDataSetChanged();
     }
 }
--- src/main/java/com/tastybug/timetracker/gui/view/ProjectView.java
+++ src/main/java/com/tastybug/timetracker/gui/view/ProjectView.java
@@ -1,23 +1,31 @@
 package com.tastybug.timetracker.gui.view;
 
+import android.app.Activity;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.common.base.Optional;
 import com.tastybug.timetracker.R;
+import com.tastybug.timetracker.database.dao.TrackingRecordDAO;
+import com.tastybug.timetracker.gui.delegate.TrackingDelegate;
 import com.tastybug.timetracker.model.Project;
 import com.tastybug.timetracker.model.TrackingConfiguration;
 import com.tastybug.timetracker.model.TrackingRecord;
 import com.tastybug.timetracker.model.statistics.StatisticProjectCompletion;
+import com.tastybug.timetracker.task.tracking.KickStopTrackingRecordTask;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-public class ProjectView extends LinearLayout {
+public class ProjectView extends LinearLayout implements View.OnClickListener {
 
+    private Project project;
+    private ImageButton trackingStartStopButton;
     private TextView projectTitleView, lastRecordSummaryView;
     private View projectAmountMeterContainer, projectAmountMeter1, projectAmountMeter2;
 
@@ -29,18 +37,42 @@ public class ProjectView extends LinearLayout {
 
         projectTitleView = (TextView) findViewById(R.id.project_title);
         lastRecordSummaryView = (TextView) findViewById(R.id.project_last_tracking_record_summary);
+        trackingStartStopButton = (ImageButton) findViewById(R.id.trackingStartStop);
         projectAmountMeterContainer = findViewById(R.id.project_amount_meter_container);
         projectAmountMeter1 = findViewById(R.id.project_amount_meter_1);
         projectAmountMeter2 = findViewById(R.id.project_amount_meter_2);
+
+        trackingStartStopButton.setOnClickListener(this);
     }
 
     public void showProject(Project project,
                             Optional<TrackingRecord> lastTrackingRecordOpt,
                             ArrayList<TrackingRecord> trackingRecords,
                             TrackingConfiguration configuration) {
+        this.project = project;
         renderProjectTitle(project);
         renderLastTrackingRecord(lastTrackingRecordOpt);
         renderProjectAmountMeter(configuration, trackingRecords);
+        renderTrackingControlButton();
+    }
+
+    public void onClick(View v) {
+        String projectUuid = project.getUuid();
+        Optional<TrackingRecord> ongoing = new TrackingRecordDAO(getContext()).getRunning(projectUuid);
+        if (ongoing.isPresent()) {
+            KickStopTrackingRecordTask.aTask(getContext()).withProjectUuid(projectUuid).execute();
+        } else {
+            TrackingDelegate.aDelegate((Activity)getContext()).startTracking(project);
+        }
+    }
+
+    private void renderTrackingControlButton() {
+        Optional<TrackingRecord> ongoingTracking = new TrackingRecordDAO(getContext()).getRunning(project.getUuid());
+        if(ongoingTracking.isPresent()) {
+            trackingStartStopButton.setImageResource(R.drawable.ic_stop_tracking);
+        } else {
+            trackingStartStopButton.setImageResource(R.drawable.ic_start_tracking);
+        }
     }
 
     private void renderProjectTitle(Project project) {
@@ -50,7 +82,13 @@ public class ProjectView extends LinearLayout {
 
     private void renderLastTrackingRecord(Optional<TrackingRecord> lastTrackingRecordOpt) {
         if(lastTrackingRecordOpt.isPresent()) {
-            lastRecordSummaryView.setText(lastTrackingRecordOpt.get().getStart().get().toString());
+            if (lastTrackingRecordOpt.get().isRunning()) {
+                lastRecordSummaryView.setText(getContext().getString(R.string.current_record_started_at_X,
+                        SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.LONG, SimpleDateFormat.SHORT).format(lastTrackingRecordOpt.get().getStart().get())));
+            } else {
+                lastRecordSummaryView.setText(getContext().getString(R.string.last_record_ended_at_X,
+                        SimpleDateFormat.getDateTimeInstance(SimpleDateFormat.LONG, SimpleDateFormat.SHORT).format(lastTrackingRecordOpt.get().getEnd().get())));
+            }
         } else {
             lastRecordSummaryView.setText("");
         }
