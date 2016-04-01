8236337bcfa430225e3e5f19e25069f53d1864fb
Philipp Bartsch <tastybug@tastybug.com>
2016-04-01 23:44:46 +0200

    minor code cleanup: moved application version accessor code back into a helper class: VersionHelper

--- src/main/java/com/tastybug/timetracker/application/Application.java
+++ src/main/java/com/tastybug/timetracker/application/Application.java
@@ -1,24 +1,28 @@
 package com.tastybug.timetracker.application;
 
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.util.Log;
 import android.widget.Toast;
 
 import com.tastybug.timetracker.task.testdata.TestDataGenerationTask;
 import com.tastybug.timetracker.util.FirstRunHelper;
+import com.tastybug.timetracker.util.VersionHelper;
 
 import net.danlew.android.joda.JodaTimeAndroid;
 
 public class Application extends android.app.Application {
 
+    private FirstRunHelper firstRunHelper;
+    private VersionHelper versionHelper;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        firstRunHelper = new FirstRunHelper(this);
+        versionHelper = new VersionHelper(this);
 
         initializeJoda();
         if (isFirstRun()) {
-            performFirstRunSetup();
+            runGenerateTestDataTask();
             declareFirstRunConsumed();
         }
 
@@ -30,50 +34,27 @@ public class Application extends android.app.Application {
     }
 
     private boolean isFirstRun() {
-        return new FirstRunHelper(this).isFirstRun();
+        return firstRunHelper.isFirstRun();
     }
 
     private void declareFirstRunConsumed() {
-        new FirstRunHelper(this).declareFirstRunConsumed();
+        firstRunHelper.declareFirstRunConsumed();
     }
 
-    private void performFirstRunSetup() {
-        runGenerateTestdataTask();
-    }
-
-    private void runGenerateTestdataTask() {
-        if (getVersionName(getPackageInfo()).contains("SNAPSHOT")) {
+    private void runGenerateTestDataTask() {
+        if (versionHelper.isDevelopmentVersion()) {
             new TestDataGenerationTask(this).execute();
         } else {
             Log.i(getClass().getSimpleName(), "Skipping test data generation,"
-                    + getVersionName(getPackageInfo()).contains("SNAPSHOT")
+                    + versionHelper.getVersionName()
                     + " is not a SNAPSHOT release!");
         }
     }
 
     private void printWelcomeToast() {
-        PackageInfo packageInfo = getPackageInfo();
         Toast.makeText(this,
-                getApplicationName(packageInfo) + " " + getVersionName(packageInfo),
+                versionHelper.getApplicationName() + " " + versionHelper.getVersionName(),
                 Toast.LENGTH_SHORT).show();
     }
 
-    private PackageInfo getPackageInfo() {
-        try {
-            PackageManager pm = getPackageManager();
-            PackageInfo pInfo = pm.getPackageInfo(getPackageName(), PackageManager.GET_PERMISSIONS);
-            return pInfo;
-        } catch (PackageManager.NameNotFoundException nnfe) {
-            Log.e(getClass().getSimpleName(), nnfe.getMessage());
-            return null;
-        }
-    }
-
-    private String getVersionName(PackageInfo packageInfo) {
-        return packageInfo.versionName;
-    }
-
-    private String getApplicationName(PackageInfo packageInfo) {
-        return getString(packageInfo.applicationInfo.labelRes);
-    }
 }
