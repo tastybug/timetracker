250b5ec38d8ac13807b69cfc7a15a6e4e53b5997
Philipp Bartsch <tastybug@tastybug.com>
2016-06-01 19:06:58 +0200

    resolved #46: show a summary toast after completion of a tracking record

--- src/main/java/com/tastybug/timetracker/gui/eventhandler/PostTrackingKickStopHandler.java
+++ src/main/java/com/tastybug/timetracker/gui/eventhandler/PostTrackingKickStopHandler.java
@@ -3,10 +3,21 @@ package com.tastybug.timetracker.gui.eventhandler;
 import android.app.Activity;
 import android.widget.Toast;
 
+import com.google.common.base.Optional;
 import com.squareup.otto.Subscribe;
+import com.tastybug.timetracker.R;
+import com.tastybug.timetracker.database.dao.ProjectDAO;
+import com.tastybug.timetracker.database.dao.TrackingConfigurationDAO;
+import com.tastybug.timetracker.database.dao.TrackingRecordDAO;
 import com.tastybug.timetracker.gui.dialog.trackingrecord.ConfirmKeepVeryShortTrackingRecordDialogFragment;
 import com.tastybug.timetracker.gui.dialog.trackingrecord.EditTrackingRecordDescriptionDialogFragment;
+import com.tastybug.timetracker.model.Project;
+import com.tastybug.timetracker.model.TrackingConfiguration;
+import com.tastybug.timetracker.model.TrackingRecord;
 import com.tastybug.timetracker.task.tracking.KickStoppedTrackingRecordEvent;
+import com.tastybug.timetracker.util.DurationFormatterFactory;
+
+import org.joda.time.Duration;
 
 /**
  * Whenever a rather short TrackingRecord has been finished, ask the user whether to keep or
@@ -32,6 +43,7 @@ public class PostTrackingKickStopHandler extends AbstractOttoEventHandler {
                     .forTrackingRecord(event.getTrackingRecord())
                     .show(activity.getFragmentManager(), getClass().getSimpleName());
         }
+        showTrackingSummary(event.getTrackingRecord());
     }
 
     private void confirmKeepVeryShortRecord(String trackingRecordUuid) {
@@ -39,5 +51,13 @@ public class PostTrackingKickStopHandler extends AbstractOttoEventHandler {
                 .aDialog()
                 .forTrackingRecordUuid(trackingRecordUuid)
                 .show(activity.getFragmentManager(), getClass().getSimpleName());
+    }
+
+    private void showTrackingSummary(TrackingRecord trackingRecord) {
+        TrackingConfiguration trackingConfiguration = new TrackingConfigurationDAO(activity).getByProjectUuid(trackingRecord.getProjectUuid()).get();
+        Project project = new ProjectDAO(activity).get(trackingRecord.getProjectUuid()).get();
+        Optional<Duration> roundedDurationOpt = trackingRecord.toEffectiveDuration(trackingConfiguration);
+        String durationString = DurationFormatterFactory.getFormatter(activity, trackingRecord.toDuration().get()).print(roundedDurationOpt.get().toPeriod());
+        Toast.makeText(activity, activity.getString(R.string.toast_X_booked_on_project_Y, durationString, project.getTitle()), Toast.LENGTH_LONG).show();
     }
 }
